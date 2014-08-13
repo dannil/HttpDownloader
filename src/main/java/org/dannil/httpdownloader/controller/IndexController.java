95e1ce41496d82e0fdf9a454ff359ee73726745e
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-08-13 18:13:37 +0200

    Added testing for log4j

--- src/main/java/org/dannil/httpdownloader/controller/IndexController.java
+++ src/main/java/org/dannil/httpdownloader/controller/IndexController.java
@@ -1,5 +1,7 @@
 package org.dannil.httpdownloader.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/index")
 public final class IndexController {
 
+	final Logger logger = LogManager.getLogger(IndexController.class);
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public final void indexGET() {
 		// Loads index.xhtml from /WEB-INF/view
+		this.logger.debug("Hello servlet!");
 		System.out.println("Loading index.xhtml...");
 	}
 	
