41ae7c43f31218062562066e501ba7edeeffe619
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-08-18 13:12:15 +0200

    Removed logging

--- src/main/java/org/dannil/httpdownloader/controller/IndexController.java
+++ src/main/java/org/dannil/httpdownloader/controller/IndexController.java
@@ -9,28 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller(value = "IndexController")
 @RequestMapping("/index")
 public final class IndexController {
-
-	final Logger logger = LogManager.getLogger(IndexController.class);
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public final void indexGET() {
-		this.logger.debug("Hello servlet!");
 		System.out.println("Loading index.xhtml...");
-		
-		System.out.println(this.logger.getLevel());
-		
-		System.out.println(logger.isErrorEnabled());
-		System.out.println(logger.isDebugEnabled());
-		
-		//logs debug message
-		if(this.logger.isDebugEnabled()){
-			this.logger.debug("getWelcome is executed!");
-		} else {
-			System.out.println("not enabled");
-		}
- 
-		//logs exception
-		//this.logger.error("This is Error message", new Exception("Testing"));
 	}
 	
 }
