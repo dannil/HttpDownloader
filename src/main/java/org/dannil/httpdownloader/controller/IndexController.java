62c623622a227116a364b946f7f44e08c1f0e63c
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-08-21 19:01:58 +0200

    Removed unused imports

--- src/main/java/org/dannil/httpdownloader/controller/IndexController.java
+++ src/main/java/org/dannil/httpdownloader/controller/IndexController.java
@@ -4,8 +4,6 @@
 
 package org.dannil.httpdownloader.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
