0d1f8fbe2939a1ecf81875a8cf2b1e8ed8915f25
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-08-23 12:35:16 +0200

    Added code for testing dynamic language selection

--- src/main/java/org/dannil/httpdownloader/controller/IndexController.java
+++ src/main/java/org/dannil/httpdownloader/controller/IndexController.java
@@ -4,6 +4,11 @@
 
 package org.dannil.httpdownloader.controller;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpSession;
+
 import org.dannil.httpdownloader.service.MarkerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +18,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/index")
 public final class IndexController {
 	
+	// Loads index.xhtml from /WEB-INF/view
 	@RequestMapping(method = RequestMethod.GET)
-	public final void indexGET() {
-		// Loads index.xhtml from /WEB-INF/view
+	public final void indexGET(final HttpSession session) {
+		ResourceBundle languageBundle = ResourceBundle.getBundle("/WEB-INF/conf/languages/language", new Locale("sv", "SE"));
+		System.out.println(languageBundle.getString("hello_world"));
+		
+		session.setAttribute("languageBundle", languageBundle);
+		
 		System.out.println("Loading " + MarkerService.VIEW_FOLDER + "/index.xhtml...");
 	}
 	
