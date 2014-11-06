99de1de020bc593f89af10d80bbf62bd11fab3cd
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-11-06 20:48:25 +0100

    Added database resources

---
+++ resources/create_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Users ( UserID BIGINT(11) AUTO_INCREMENT PRIMARY KEY,
+					 Email VARCHAR(64) NOT NULL,
+					 Password VARCHAR(320) NOT NULL,
+					 Firstname VARCHAR(32) NOT NULL,
+					 Lastname VARCHAR(32) NOT NULL
+				   );
+				   
+CREATE TABLE Downloads ( DownloadID BIGINT(11) AUTO_INCREMENT PRIMARY KEY,
+						 Title VARCHAR(32) NOT NULL,
+						 StartDate DATE NOT NULL,
+						 EndDate DATE NOT NULL,
+						 Data BLOB NOT NULL
+					   );
+
+CREATE TABLE UsersDownloads ( UserID BIGINT(11) NOT NULL,
+                              DownloadID BIGINT(11) NOT NULL,
+                              PRIMARY KEY (UserID, DownloadID),
+                              FOREIGN KEY (UserID) REFERENCES Users(UserID),
+                              FOREIGN KEY (DownloadID) REFERENCES Downloads(DownloadID)
+                              );
