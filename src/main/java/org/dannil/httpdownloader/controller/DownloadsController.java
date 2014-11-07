de7c26a85dd1416b22727c11b4540b914a72db11
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-11-07 15:45:33 +0100

    Added logic for URL handling

--- src/main/java/org/dannil/httpdownloader/controller/DownloadsController.java
+++ src/main/java/org/dannil/httpdownloader/controller/DownloadsController.java
@@ -3,6 +3,7 @@ package org.dannil.httpdownloader.controller;
 import javax.servlet.http.HttpSession;
 
 import org.dannil.httpdownloader.utility.PathUtility;
+import org.dannil.httpdownloader.utility.RedirectUtility;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +14,30 @@ public final class DownloadsController extends GenericController {
 
 	// Loads downloads.xhtml from /WEB-INF/view
 	@RequestMapping(method = RequestMethod.GET)
-	public final void downloadsGET(final HttpSession session) {
+	public final String downloadsGET(final HttpSession session) {
+		if (session.getAttribute("user") == null) {
+			System.out.println("IS NULL");
+			return RedirectUtility.redirect(PathUtility.URL_LOGIN);
+		}
+
 		System.out.println("Loading " + PathUtility.VIEW_PATH + "/downloads.xhtml...");
 		session.setAttribute("language", super.languageBundle);
+
+		return PathUtility.URL_DOWNLOADS;
+	}
+
+	// Loads add.xhtml from /WEB-INF/view/downloads
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public final String downloadsAddGET(final HttpSession session) {
+		if (session.getAttribute("user") == null) {
+			System.out.println("IS NULL");
+			return RedirectUtility.redirect(PathUtility.URL_LOGIN);
+		}
+
+		System.out.println("Loading " + PathUtility.VIEW_DOWNLOADS_PATH + "/add.xhtml...");
+		session.setAttribute("language", super.languageBundle);
+
+		return PathUtility.URL_DOWNLOADS_ADD;
 	}
 
 }
--- src/main/java/org/dannil/httpdownloader/utility/PathUtility.java
+++ src/main/java/org/dannil/httpdownloader/utility/PathUtility.java
@@ -16,10 +16,19 @@ public final class PathUtility {
 
 	// View folder
 	public static final String VIEW_PATH = WEB_ROOT + "/view";
+	public static final String VIEW_DOWNLOADS_PATH = VIEW_PATH + "/downloads";
 
 	// Languages folder
 	public static final String LANGUAGES_PATH = CONFIGURATION_PATH + "/languages";
 	public static final String LANGUAGE_PATH = LANGUAGES_PATH + "/language";
+
+	// URL inside webapp
+	public static final String URL_DOWNLOADS = "/downloads";
+	public static final String URL_DOWNLOADS_ADD = URL_DOWNLOADS + "/add";
+
+	public static final String URL_INDEX = "/index";
+
+	public static final String URL_LOGIN = "/login";
 
 	/**
 	 * Private constructor to make the class a true singleton
