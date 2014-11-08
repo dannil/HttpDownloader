226fd3fc14ebeb3d6f86ba9666032903d21d7d7f
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-11-08 11:02:37 +0100

    Added conversion methods

--- src/main/java/org/dannil/httpdownloader/utility/LanguageUtility.java
+++ src/main/java/org/dannil/httpdownloader/utility/LanguageUtility.java
@@ -25,7 +25,7 @@ public final class LanguageUtility {
 	}
 
 	/**
-	 * Private constructor to make the class a true singleton
+	 * Private constructor to make the class a singleton.
 	 */
 	private LanguageUtility() {
 		throw new AssertionError();
@@ -33,6 +33,7 @@ public final class LanguageUtility {
 
 	/**
 	 * Returns a bundle with the language which matches the users current display language.
+	 * 
 	 * @return ResourceBundle with default language
 	 */
 	public static final ResourceBundle getLanguageBundle() {
@@ -42,8 +43,10 @@ public final class LanguageUtility {
 	/**
 	 * Returns a bundle with the language which matches the inputed locale.
 	 * If the language file doesn't exist, return a standard language (enUS).
+	 * 
 	 * @param language
 	 * 					- The language to load
+	 * 
 	 * @return ResourceBundle with inputed locale
 	 */
 	public static final ResourceBundle getLanguageBundle(final Locale language) {
@@ -51,6 +54,30 @@ public final class LanguageUtility {
 			return ResourceBundle.getBundle(PathUtility.LANGUAGE_PATH, language);
 		}
 		return ResourceBundle.getBundle(PathUtility.LANGUAGE_PATH, DEFAULT_LOCALE);
+	}
+
+	/**
+	 * Convert a locale to its IETF BCP 47 string representation.
+	 * 
+	 * @param language
+	 * 					- The locale to be converted
+	 * 
+	 * @return A String of the locale in IETF BCP 47 language tag representation
+	 */
+	public static final String toString(Locale language) {
+		return language.toLanguageTag();
+	}
+
+	/**
+	 * Convert a language string in IETF BCP 47 representation to the correct corresponding locale.
+	 * 
+	 * @param language
+	 * 					- The string to be converted
+	 * 
+	 * @return A Locale converted from the language string
+	 */
+	public static final Locale toLocale(String language) {
+		return Locale.forLanguageTag(language);
 	}
 
 }
