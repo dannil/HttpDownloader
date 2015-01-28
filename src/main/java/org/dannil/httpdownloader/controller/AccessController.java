19e2b9a330996cf650d04b4f02fc974ef08e2387
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-01-28 16:08:35 +0100

    Removed output

--- src/main/java/org/dannil/httpdownloader/controller/AccessController.java
+++ src/main/java/org/dannil/httpdownloader/controller/AccessController.java
@@ -63,8 +63,6 @@ public final class AccessController {
 		download.setUser(tempUser);
 		final Download tempDownload = this.downloadService.save(download);
 		user.addDownload(tempDownload);
-
-		System.out.println(PathUtility.getAbsolutePathToLanguage());
 	}
 
 	// Login a user, loads login.xhtml from /WEB-INF/view
