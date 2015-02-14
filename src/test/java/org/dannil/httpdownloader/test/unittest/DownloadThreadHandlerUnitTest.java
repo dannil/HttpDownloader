a3677ab8babd701996ebe65d7b633824410103d4
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-02-14 13:50:18 +0100

    Added tests

--- src/test/java/org/dannil/httpdownloader/test/unittest/DownloadThreadHandlerUnitTest.java
+++ src/test/java/org/dannil/httpdownloader/test/unittest/DownloadThreadHandlerUnitTest.java
@@ -1,8 +1,12 @@
 package org.dannil.httpdownloader.test.unittest;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.dannil.httpdownloader.handler.DownloadThreadHandler;
 import org.dannil.httpdownloader.model.Download;
 import org.dannil.httpdownloader.test.utility.TestUtility;
+import org.dannil.httpdownloader.utility.FileUtility;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,27 +30,37 @@ public class DownloadThreadHandlerUnitTest {
 	// DOES NOT CURRENTLY WORK; NEEDS REFACTORING
 
 	@Test
-	public final void threadHandlerSaveToDiskNotNullDownload() {
+	public final void saveToDiskNotNullDownload() {
 		final Download download = new Download(TestUtility.getDownload());
 
 		this.downloadThreadHandler.saveToDisk(download);
 	}
 
 	@Test(expected = NullPointerException.class)
-	public final void threadHandlerSaveToDiskNullDownload() {
+	public final void saveToDiskNullDownload() {
 		this.downloadThreadHandler.saveToDisk(null);
 	}
 
+	// WORKS
 	@Test
-	public final void threadHandlerDeleteFromDiskNotNullDownload() {
+	public final void deleteFromDiskNotNullDownload() throws IOException {
 		final Download download = new Download(TestUtility.getDownload());
+
+		File file = FileUtility.getFileFromURL(download);
+		FileUtility.saveToDrive(file);
 
 		this.downloadThreadHandler.deleteFromDisk(download);
 	}
 
 	@Test(expected = NullPointerException.class)
-	public final void threadHandlerDeleteFromDiskNullDownload() {
+	public final void deleteFromDiskNullDownload() {
 		this.downloadThreadHandler.deleteFromDisk(null);
+	}
+
+	@Test
+	public final void initializeHandlerTwice() {
+		final DownloadThreadHandler handler1 = new DownloadThreadHandler();
+		final DownloadThreadHandler handler2 = new DownloadThreadHandler();
 	}
 
 }
