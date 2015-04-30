c9a2cb3ddfab8058643cdbe628875c5490de391f
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-04-30 10:37:31 +0200

    Added coverity scan script for Windows platform

---
+++ resources/run_coverity.bat
@@ -0,0 +1,9 @@
+﻿SET version=1.0.0
+
+cd ../
+
+cov-build --dir cov-int mvn clean
+cov-build --dir cov-int mvn install
+cov-emit-java --dir cov-int --war target/httpdownloader-%version%.war
+
+jar -cMf coverity-httpdownloader-%version%.zip cov-int
