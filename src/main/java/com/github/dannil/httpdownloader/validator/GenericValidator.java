fad0904bd091cf5b2e79b08ef9f0d853b83d89d0
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-02-15 18:16:40 +0100

    Removed unnecessary branch

--- src/main/java/com/github/dannil/httpdownloader/validator/GenericValidator.java
+++ src/main/java/com/github/dannil/httpdownloader/validator/GenericValidator.java
@@ -6,9 +6,9 @@ import org.apache.commons.validator.routines.EmailValidator;
  * Class which encapsulates generic validation methods which isn't
  * specific to any other class.
  * 
- * @author      Daniel Nilsson (daniel.nilsson @ dannils.se)
- * @version     1.0.0
- * @since       0.0.1-SNAPSHOT
+ * @author Daniel Nilsson (daniel.nilsson @ dannils.se)
+ * @version 1.0.0
+ * @since 0.0.1-SNAPSHOT
  */
 public class GenericValidator {
 
@@ -20,7 +20,7 @@ public class GenericValidator {
 	 * Validate the specified string as containing only letters.
 	 * 
 	 * @param input
-	 * 				the string to validate
+	 *            the string to validate
 	 * 
 	 * @return true if the string contains only letters, false if not
 	 */
@@ -36,7 +36,7 @@ public class GenericValidator {
 	 * Validate the specified string as containing only numbers.
 	 * 
 	 * @param input
-	 * 				the string to validate
+	 *            the string to validate
 	 * 
 	 * @return true if the string contains only numbers, false if not
 	 */
@@ -51,16 +51,14 @@ public class GenericValidator {
 	 * Validate the specified string as a valid e-mail format.
 	 * 
 	 * @param email
-	 * 				the string to validate as a e-mail
+	 *            the string to validate as a e-mail
 	 * 
 	 * @return true if the string is a valid e-mail format, false if not
 	 */
 	protected final boolean isValidEmail(final String email) {
-		if (email == null) {
-			return false;
-		}
 		EmailValidator validator = EmailValidator.getInstance();
 		return validator.isValid(email);
+		// return validator.isValid(email);
 		// return
 		// email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 	}
