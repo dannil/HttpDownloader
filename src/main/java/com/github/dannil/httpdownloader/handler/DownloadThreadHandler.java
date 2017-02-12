aea889445f85ca4e8549bd7fdd2085bd11e64447
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-02-12 09:57:19 +0100

    Fixes reported by SonarQube

--- src/main/java/com/github/dannil/httpdownloader/handler/DownloadThreadHandler.java
+++ src/main/java/com/github/dannil/httpdownloader/handler/DownloadThreadHandler.java
@@ -2,7 +2,7 @@ package com.github.dannil.httpdownloader.handler;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -19,7 +19,7 @@ import com.github.dannil.httpdownloader.utility.FileUtility;
  * Middleware class that acts between the service layer and the persistence layer. This
  * class makes sure that several downloads can be initiated at once, each one in a
  * separate thread.
- * 
+ *
  * @author Daniel Nilsson (daniel.nilsson94 @ outlook.com)
  * @version 1.0.1-SNAPSHOT
  * @since 1.0.0
@@ -27,136 +27,136 @@ import com.github.dannil.httpdownloader.utility.FileUtility;
 @Component
 public class DownloadThreadHandler {
 
-	private static final Logger LOGGER = Logger.getLogger(DownloadThreadHandler.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(DownloadThreadHandler.class.getName());
 
-	private static DownloadThreadHandler downloadThreadHandlerInstance;
+    private static DownloadThreadHandler downloadThreadHandlerInstance;
 
-	private List<Thread> threads;
+    private List<Thread> threads;
 
-	@Autowired
-	private DownloadSaveToDisk saveToDiskInstance;
+    @Autowired
+    private DownloadSaveToDisk saveToDiskInstance;
 
-	@Autowired
-	private DownloadDeleteFromDisk deleteFromDiskInstance;
+    @Autowired
+    private DownloadDeleteFromDisk deleteFromDiskInstance;
 
-	private DownloadThreadHandler() {
-		this.threads = new LinkedList<Thread>();
-	}
+    private DownloadThreadHandler() {
+        this.threads = new ArrayList<>();
+    }
 
-	public static synchronized DownloadThreadHandler getInstance() {
-		if (downloadThreadHandlerInstance == null) {
-			downloadThreadHandlerInstance = new DownloadThreadHandler();
-		}
-		return downloadThreadHandlerInstance;
-	}
+    public static synchronized DownloadThreadHandler getInstance() {
+        if (downloadThreadHandlerInstance == null) {
+            downloadThreadHandlerInstance = new DownloadThreadHandler();
+        }
+        return downloadThreadHandlerInstance;
+    }
 
-	public synchronized void saveToDisk(Download download) {
-		if (download == null) {
-			throw new IllegalArgumentException("Download can't be null");
-		}
+    public synchronized void saveToDisk(Download download) {
+        if (download == null) {
+            throw new IllegalArgumentException("Download can't be null");
+        }
 
-		this.saveToDiskInstance.setDownload(download);
+        this.saveToDiskInstance.setDownload(download);
 
-		Thread t = new Thread(this.saveToDiskInstance, download.getFormat());
+        Thread t = new Thread(this.saveToDiskInstance, download.getFormat());
 
-		this.threads.add(t);
+        this.threads.add(t);
 
-		t.start();
-	}
+        t.start();
+    }
 
-	public synchronized void deleteFromDisk(Download download) {
-		if (download == null) {
-			throw new IllegalArgumentException("Download can't be null");
-		}
+    public synchronized void deleteFromDisk(Download download) {
+        if (download == null) {
+            throw new IllegalArgumentException("Download can't be null");
+        }
 
-		this.deleteFromDiskInstance.setDownload(download);
+        this.deleteFromDiskInstance.setDownload(download);
 
-		Thread t = new Thread(this.deleteFromDiskInstance, download.getFormat());
+        Thread t = new Thread(this.deleteFromDiskInstance, download.getFormat());
 
-		this.threads.add(t);
+        this.threads.add(t);
 
-		t.start();
-	}
+        t.start();
+    }
 
-	/**
-	 * Interrupts a thread with the specified name.
-	 * 
-	 * @param threadName
-	 *            the name of the thread to interrupt
-	 */
-	public synchronized void interrupt(String threadName) {
-		for (Thread t : this.threads) {
-			if (t.getName().equals(threadName)) {
-				LOGGER.info("Found thread " + threadName + ", interrupting...");
-				t.interrupt();
+    /**
+     * Interrupts a thread with the specified name.
+     *
+     * @param threadName
+     *            the name of the thread to interrupt
+     */
+    public synchronized void interrupt(String threadName) {
+        for (Thread t : this.threads) {
+            if (t.getName().equals(threadName)) {
+                LOGGER.info("Found thread " + threadName + ", interrupting...");
+                t.interrupt();
 
-				this.threads.remove(t);
-				break;
-			}
-		}
-	}
+                this.threads.remove(t);
+                break;
+            }
+        }
+    }
 
 }
 
 @Component
 class DownloadSaveToDisk implements Runnable {
 
-	private static final Logger LOGGER = Logger.getLogger(DownloadSaveToDisk.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(DownloadSaveToDisk.class.getName());
 
-	@Autowired
-	private DownloadRepository repository;
+    @Autowired
+    private DownloadRepository repository;
 
-	private Download download;
+    private Download download;
 
-	private DownloadSaveToDisk() {
+    private DownloadSaveToDisk() {
 
-	}
+    }
 
-	@Override
-	public void run() {
-		LOGGER.info("Trying to save download " + this.download.getFormat());
+    @Override
+    public void run() {
+        LOGGER.info("Trying to save download " + this.download.getFormat());
 
-		File file;
-		try {
-			file = FileUtility.getFileFromURL(this.download);
-			FileUtility.saveToDrive(file);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        File file;
+        try {
+            file = FileUtility.getFileFromURL(this.download);
+            FileUtility.saveToDrive(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		this.download.setEndDate(new DateTime());
-		this.repository.save(this.download);
-	}
+        this.download.setEndDate(new DateTime());
+        this.repository.save(this.download);
+    }
 
-	public void setDownload(final Download download) {
-		this.download = download;
-	}
+    public void setDownload(final Download download) {
+        this.download = download;
+    }
 
 }
 
 @Component
 class DownloadDeleteFromDisk implements Runnable {
 
-	private static final Logger LOGGER = Logger.getLogger(DownloadSaveToDisk.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(DownloadSaveToDisk.class.getName());
 
-	private Download download;
+    private Download download;
 
-	private DownloadDeleteFromDisk() {
+    private DownloadDeleteFromDisk() {
 
-	}
+    }
 
-	@Override
-	public void run() {
-		LOGGER.info("Trying to delete download " + this.download.getFormat());
+    @Override
+    public void run() {
+        LOGGER.info("Trying to delete download " + this.download.getFormat());
 
-		boolean isDeleted = FileUtility.deleteFromDrive(this.download);
-		if (!isDeleted) {
-			throw new DownloadException("Couldn't delete download " + this.download);
-		}
-	}
+        boolean isDeleted = FileUtility.deleteFromDrive(this.download);
+        if (!isDeleted) {
+            throw new DownloadException("Couldn't delete download " + this.download);
+        }
+    }
 
-	public void setDownload(final Download download) {
-		this.download = download;
-	}
+    public void setDownload(final Download download) {
+        this.download = download;
+    }
 
 }
--- src/main/java/com/github/dannil/httpdownloader/model/User.java
+++ src/main/java/com/github/dannil/httpdownloader/model/User.java
@@ -22,7 +22,7 @@ import org.springframework.stereotype.Component;
 /**
  * Encapsulates all information needed about an user. Also contains logic for associating
  * a download with a specific user, deleting a fetching a download.
- * 
+ *
  * @author Daniel Nilsson (daniel.nilsson94 @ outlook.com)
  * @version 1.0.1-SNAPSHOT
  * @since 0.0.1-SNAPSHOT
@@ -32,240 +32,240 @@ import org.springframework.stereotype.Component;
 @Table(name = "users")
 public class User implements Serializable {
 
-	private static final long serialVersionUID = 5312673884211830942L;
+    private static final long serialVersionUID = 5312673884211830942L;
 
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "UserID")
-	@JoinColumn(name = "UserID")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "UserID")
+    @JoinColumn(name = "UserID")
+    private Long id;
 
-	@Column(name = "Email", length = 128, nullable = false)
-	@NotNull
-	private String email;
+    @Column(name = "Email", length = 128, nullable = false)
+    @NotNull
+    private String email;
 
-	@Column(name = "Password", length = 512, nullable = false)
-	@NotNull
-	private String password;
+    @Column(name = "Password", length = 512, nullable = false)
+    @NotNull
+    private String password;
 
-	@Column(name = "Firstname", length = 50, nullable = false)
-	@NotNull
-	private String firstname;
+    @Column(name = "Firstname", length = 50, nullable = false)
+    @NotNull
+    private String firstname;
 
-	@Column(name = "Lastname", length = 50, nullable = false)
-	@NotNull
-	private String lastname;
+    @Column(name = "Lastname", length = 50, nullable = false)
+    @NotNull
+    private String lastname;
 
-	@OneToMany(mappedBy = "user", fetch = EAGER)
-	private Collection<Download> downloads;
+    @OneToMany(mappedBy = "user", fetch = EAGER)
+    private Collection<Download> downloads;
 
-	/**
-	 * Private constructor
-	 */
-	private User() {
-		// Needed for Spring Framework to enable autowire
+    /**
+     * Private constructor
+     */
+    private User() {
+        // Constructor needed for Spring Framework to enable autowire
 
-		this.downloads = new ArrayList<Download>();
-	}
+        this.downloads = new ArrayList<>();
+    }
 
-	/**
-	 * Overloaded constructor
-	 * 
-	 * @param email
-	 *            the users email
-	 * @param password
-	 *            the users password
-	 * @param firstname
-	 *            the users firstname
-	 * @param lastname
-	 *            the users lastname
-	 */
-	public User(String email, String password, String firstname, String lastname) {
-		this();
-		this.email = Objects.requireNonNull(email);
-		this.password = Objects.requireNonNull(password);
-		this.firstname = Objects.requireNonNull(firstname);
-		this.lastname = Objects.requireNonNull(lastname);
-	}
+    /**
+     * Overloaded constructor
+     *
+     * @param email
+     *            the users email
+     * @param password
+     *            the users password
+     * @param firstname
+     *            the users firstname
+     * @param lastname
+     *            the users lastname
+     */
+    public User(String email, String password, String firstname, String lastname) {
+        this();
+        this.email = Objects.requireNonNull(email);
+        this.password = Objects.requireNonNull(password);
+        this.firstname = Objects.requireNonNull(firstname);
+        this.lastname = Objects.requireNonNull(lastname);
+    }
 
-	/**
-	 * Overloaded constructor
-	 * 
-	 * @param email
-	 *            the users email
-	 * @param password
-	 *            the users password
-	 * @param firstname
-	 *            the users firstname
-	 * @param lastname
-	 *            the users lastname
-	 * @param downloads
-	 *            the users downloads
-	 */
-	public User(String email, String password, String firstname, String lastname, Collection<Download> downloads) {
-		this(email, password, firstname, lastname);
+    /**
+     * Overloaded constructor
+     *
+     * @param email
+     *            the users email
+     * @param password
+     *            the users password
+     * @param firstname
+     *            the users firstname
+     * @param lastname
+     *            the users lastname
+     * @param downloads
+     *            the users downloads
+     */
+    public User(String email, String password, String firstname, String lastname, Collection<Download> downloads) {
+        this(email, password, firstname, lastname);
 
-		for (Download d : downloads) {
-			addDownload(d);
-		}
-	}
+        for (Download d : downloads) {
+            addDownload(d);
+        }
+    }
 
-	public Long getId() {
-		return this.id;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getEmail() {
-		return this.email;
-	}
+    public String getEmail() {
+        return this.email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getPassword() {
-		return this.password;
-	}
+    public String getPassword() {
+        return this.password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getFirstname() {
-		return this.firstname;
-	}
+    public String getFirstname() {
+        return this.firstname;
+    }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-	public String getLastname() {
-		return this.lastname;
-	}
+    public String getLastname() {
+        return this.lastname;
+    }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
-	public Collection<Download> getDownloads() {
-		return this.downloads;
-	}
+    public Collection<Download> getDownloads() {
+        return this.downloads;
+    }
 
-	public void setDownloads(Collection<Download> downloads) {
-		this.downloads = downloads;
-	}
+    public void setDownloads(Collection<Download> downloads) {
+        this.downloads = downloads;
+    }
 
-	/**
-	 * Add a download to the user. Performs a null check on the download before adding it.
-	 * 
-	 * @param download
-	 *            the download to add
-	 */
-	public void addDownload(Download download) {
-		if (download == null) {
-			return;
-		}
-		if (download.getId() == null) {
-			throw new IllegalArgumentException("ID can't be null");
-		}
-		download.setUser(this);
-		this.downloads.add(download);
-	}
+    /**
+     * Add a download to the user. Performs a null check on the download before adding it.
+     *
+     * @param download
+     *            the download to add
+     */
+    public void addDownload(Download download) {
+        if (download == null) {
+            return;
+        }
+        if (download.getId() == null) {
+            throw new IllegalArgumentException("ID can't be null");
+        }
+        download.setUser(this);
+        this.downloads.add(download);
+    }
 
-	/**
-	 * Delete the download with the specified id
-	 * 
-	 * @param id
-	 *            the id of the download to delete
-	 * 
-	 * @see com.github.dannil.httpdownloader.model.User#getDownload(long)
-	 */
-	public void deleteDownload(long id) {
-		deleteDownload(getDownload(id));
-	}
+    /**
+     * Delete the download with the specified id
+     *
+     * @param id
+     *            the id of the download to delete
+     *
+     * @see com.github.dannil.httpdownloader.model.User#getDownload(long)
+     */
+    public void deleteDownload(long id) {
+        deleteDownload(getDownload(id));
+    }
 
-	/**
-	 * Delete the specified download from the users downloads list. Performs a null check
-	 * on the download before searching the list for it.
-	 * 
-	 * @param download
-	 *            the download to delete
-	 */
-	public void deleteDownload(Download download) {
-		if (download == null) {
-			return;
-		}
-		if (download.getId() == null) {
-			throw new IllegalArgumentException("ID can't be null");
-		}
-		Download d = getDownload(download.getId());
-		this.downloads.remove(d);
-	}
+    /**
+     * Delete the specified download from the users downloads list. Performs a null check
+     * on the download before searching the list for it.
+     *
+     * @param download
+     *            the download to delete
+     */
+    public void deleteDownload(Download download) {
+        if (download == null) {
+            return;
+        }
+        if (download.getId() == null) {
+            throw new IllegalArgumentException("ID can't be null");
+        }
+        Download d = getDownload(download.getId());
+        this.downloads.remove(d);
+    }
 
-	/**
-	 * Return a download with the specified download ID. Performs a null check on the
-	 * users downloads list before fetching from it.
-	 * 
-	 * @param id
-	 *            the id of the download
-	 * 
-	 * @return a download with the specified id
-	 */
-	public Download getDownload(long id) {
-		if (this.downloads.isEmpty()) {
-			return null;
-		}
-		for (Download d : this.downloads) {
-			if (Objects.equals(id, d.getId())) {
-				return d;
-			}
-		}
-		return null;
-	}
+    /**
+     * Return a download with the specified download ID. Performs a null check on the
+     * users downloads list before fetching from it.
+     *
+     * @param id
+     *            the id of the download
+     *
+     * @return a download with the specified id
+     */
+    public Download getDownload(long id) {
+        if (this.downloads.isEmpty()) {
+            return null;
+        }
+        for (Download d : this.downloads) {
+            if (Objects.equals(id, d.getId())) {
+                return d;
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(this.email, this.password, this.firstname, this.lastname, this.downloads);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.email, this.password, this.firstname, this.lastname, this.downloads);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof User)) {
-			return false;
-		}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
 
-		User other = (User) obj;
-		return Objects.equals(this.email, other.email) && Objects.equals(this.password, other.password)
-				&& Objects.equals(this.firstname, other.firstname) && Objects.equals(this.lastname, other.lastname)
-				&& Objects.equals(this.downloads, other.downloads);
-	}
+        User other = (User) obj;
+        return Objects.equals(this.email, other.email) && Objects.equals(this.password, other.password)
+                && Objects.equals(this.firstname, other.firstname) && Objects.equals(this.lastname, other.lastname)
+                && Objects.equals(this.downloads, other.downloads);
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder(64);
-		builder.append("User [id=");
-		builder.append(this.id);
-		builder.append(", email=");
-		builder.append(this.email);
-		builder.append(", password=");
-		builder.append(this.password);
-		builder.append(", firstname=");
-		builder.append(this.firstname);
-		builder.append(", lastname=");
-		builder.append(this.lastname);
-		builder.append(", downloads=");
-		builder.append(this.downloads);
-		builder.append(']');
-		return builder.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(64);
+        builder.append("User [id=");
+        builder.append(this.id);
+        builder.append(", email=");
+        builder.append(this.email);
+        builder.append(", password=");
+        builder.append(this.password);
+        builder.append(", firstname=");
+        builder.append(this.firstname);
+        builder.append(", lastname=");
+        builder.append(this.lastname);
+        builder.append(", downloads=");
+        builder.append(this.downloads);
+        builder.append(']');
+        return builder.toString();
+    }
 
 }
